ce266b5d4be3c320f2774422518464db8630d983
rakesh <rakeshdr543@gmail.com>
2020-12-26 07:52:53 +0530

    Added Header Footer

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import {Container} from 'react-bootstrap'
+function App() {
+  return (
+   <>
+    <Header />
+    <main className='py-3'>
+      <Container >
+        <h1>Welcome to Order Online</h1>
+      </Container>
+    </main>
+    <Footer />
+   </>
+  );
+}
+
+export default App;
