839c1f36b18bf1e38bcfc950bff1860f67030945
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-09 16:47:34 +0530

    Added error handling backend

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,18 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
 
 const app= express()
-app.get('/',(req,res)=>{
-    res.send('Server home page')
-})
+app.use('/api/products',productRoutes)
 
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
-
-app.get('/api/products/:id',(req,res)=>{
-    const product = products.find((product)=>product._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(5000,console.log(`server is running in ${process.env.NODE_ENV} at port ${PORT}`.yellow.bold))
