f46d07a47d119f4813dc070cd9142f936309b8f5
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-16 20:38:08 +0530

    Added product controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express'
 const router = express.Router()
-import {protect} from '../middleware/authMiddleware.js'
-import {getProducts,getProductById} from '../controllers/productController.js'
+import {admin, protect} from '../middleware/authMiddleware.js'
+import {getProducts,getProductById, createProduct, createProductReview, getTopProducts, deleteProduct, updateProduct} from '../controllers/productController.js'
 
 
-router.route('/').post().get(protect,getProducts)
-router.route('/:id').get(protect,getProductById)
+router.route('/').get(getProducts).post(protect,admin,createProduct)
+router.route('/:id/reviews').post(protect,createProductReview)
+router.get('/top',getTopProducts)
+router.route('/:id').get(getProductById)
+                    .delete(protect,admin,deleteProduct)
+                    .put(protect,admin,updateProduct)
 
 
 export default router
