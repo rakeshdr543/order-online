070487875f7a8cb9208382630cc1b221042ba422
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-16 20:56:25 +0530

    added user routes controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import {protect} from '../middleware/authMiddleware.js'
-import {authUser,registerUser,getUserProfile,updateUserProfile} from '../controllers/userController.js'
+import {admin, protect} from '../middleware/authMiddleware.js'
+import {authUser,registerUser,getUserProfile,updateUserProfile, getUsers, deleteUser, getUserById, updateUser} from '../controllers/userController.js'
 
 const router = express.Router()
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect,admin,getUsers)
 router.post('/login',authUser)
 router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
+router.route('/:id').delete(protect,admin,deleteUser)
+.get(protect,admin,getUserById)
+.put(protect,admin,updateUser)
 
 export default router
