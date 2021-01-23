3cd3f18486c4b062ce0d660346029f1484bd0c23
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-23 08:05:29 +0530

    Added product deatil apge with review section

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import {productDetailsReducer, productListReducer}  from './reducers/productReducers'
+import {productDetailsReducer, productListReducer, productReviewCreateReducer}  from './reducers/productReducers'
+import {userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList:productListReducer,
-    productDetails:productDetailsReducer
+    productDetails:productDetailsReducer,
+    productReviewCreate: productReviewCreateReducer,
+    userLogin: userLoginReducer,
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer,
 })
 
 const middleware = [thunk]
