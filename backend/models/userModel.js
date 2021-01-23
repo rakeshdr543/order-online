ed82f2c37ed574dd9a0f86b0f560b5a1b89273b3
rakeshdr543 <rakeshdr543@gmail.com>
2021-01-23 15:19:11 +0530

    Fixed password hash issue

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,18 +10,18 @@ const userSchema = mongoose.Schema({
     timestamps:true
 })
 
-userSchema.methods.matchPassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword,this.password)
-}
-
-userSchema.pre('save',async function(next){
-    if(!this.isModified('password')){
-        next()
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+  }
+  
+  userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+      next()
     }
-
+  
     const salt = await bcrypt.genSalt(10)
-    this.password = bcrypt.hash(this.password,salt)
-})
+    this.password = await bcrypt.hash(this.password, salt)
+  })
 
 const User= mongoose.model('User',userSchema)
  export default User
